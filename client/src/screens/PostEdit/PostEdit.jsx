fde81385c2b5251063f48bc6260fe845d6d5c725
ginestah <68362184+ginestah@users.noreply.github.com>
2021-03-03 13:49:31 -0500

    added handleChange to PostEdit.jsx

--- client/src/screens/PostEdit/PostEdit.jsx
+++ client/src/screens/PostEdit/PostEdit.jsx
@@ -1,90 +1,97 @@
-import { useState, useEffect } from "react"
-import "./PostEdit.css"
-import { useParams, Redirect } from "react-router-dom"
-import Layout from "../../components/shared/Layout"
-import { getPost, updatePost } from "../../services/posts"
+import { useState, useEffect } from "react";
+import "./PostEdit.css";
+import { useParams, Redirect } from "react-router-dom";
+import Layout from "../../components/shared/Layout/Layout";
+import { getPost, updatePost } from "../../services/posts";
 
 const PostEdit = (props) => {
   const [post, setPost] = useState({
     author: "",
     title: "",
-    content:"",
-    imgURL: ""
-    
-  })
+    content: "",
+    imgURL: "",
+  });
 
-const [isUpdated, setUpdated] = useState(false)
-const { id } = useParams()
+  const [isUpdated, setUpdated] = useState(false);
+  const { id } = useParams();
 
-useEffect(() => {
-  const fetchPost = async () => {
-    const post = await getPost(id)
-    setPost(post)
+  useEffect(() => {
+    const fetchPost = async () => {
+      const post = await getPost(id);
+      setPost(post);
+    };
+    fetchPost();
+  }, [id]);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let { id } = props.match.params;
+    const updated = await updatePost(id, post);
+    setUpdated(updated);
+  };
+
+  if (isUpdated) {
+    return <Redirect to={`/posts/${props.match.params.id}`} />;
   }
-fetchPost()
-},[id])
-
-const handleSubmit = async (event) => {
-  event.preventDefault()
-  let { id } = props.match.params
-  const updated = await updatePost(id, post)
-  setUpdated(updated)
-}
-  
-if (isUpdated) {
-  return <Redirect to={`/posts/${props.match.params.id}`} />
-}
 
   return (
-<Layout user={props.user}>
-            <div className="post-edit">
-                <div className="image-container">
-                    <img className="edit-post-image" src={post.imgURL} alt={post.title} />
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            className="edit-input-image-link"
-                            placeholder='Image Link'
-                            value={post.imgURL}
-                            name='imgURL'
-                            required
-                            onChange={handleChange}
-                        />
-                    </form>
-                </div>
-                <form className="edit-form" onSubmit={handleSubmit}>
-                    <input
-                        className="input-author"
-                        placeholder='author'
-                        value={post.author}
-                        name='author'
-                        required
-                        autoFocus
-                        onChange={handleChange}
-                    />
-                    <input
-                        className="input-title"
-                        placeholder='Title'
-                        value={post.title}
-                        name='title'
-                        required
-                        onChange={handleChange}
-                    />
-                    <textarea
-                        className="textarea-content"
-                        rows={10}
-                        cols={78}
-                        placeholder='Content'
-                        value={post.content}
-                        name='Content'
-                        required
-                        onChange={handleChange}
-                    />
-                    <button type='submit' className="save-button">Save</button>
-                </form>
-            </div>
-        </Layout>
-
-)
-}
+    <Layout user={props.user}>
+      <div className="post-edit">
+        <div className="image-container">
+          <img className="edit-post-image" src={post.imgURL} alt={post.title} />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="edit-input-image-link"
+              placeholder="Image Link"
+              value={post.imgURL}
+              name="imgURL"
+              required
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+        <form className="edit-form" onSubmit={handleSubmit}>
+          <input
+            className="input-author"
+            placeholder="author"
+            value={post.author}
+            name="author"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <input
+            className="input-title"
+            placeholder="Title"
+            value={post.title}
+            name="title"
+            required
+            onChange={handleChange}
+          />
+          <textarea
+            className="textarea-content"
+            rows={10}
+            cols={78}
+            placeholder="Content"
+            value={post.content}
+            name="Content"
+            required
+            onChange={handleChange}
+          />
+          <button type="submit" className="save-button">
+            Save
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
 
 export default PostEdit;
